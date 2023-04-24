c952b04126688e11657afa033c1d50079579133b
hodz <elkhemlichi.mehdi3@gmail.com>
2023-04-24 14:33:33 -0700

    fix main

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 /**
  * main - Entry point
- * by Ancient-mahou & Mibonacci
  *
  * Return: Always 0
  */
@@ -39,8 +38,6 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	/** _printf("Unknown:[%r]\n");
-	 * printf("Unknown:[%r]\n");
-	 */
+
 	return (0);
 }
